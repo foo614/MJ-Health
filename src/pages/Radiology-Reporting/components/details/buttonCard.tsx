0df48acc9aef27812cd595ed346018d07c443504
LD <fld614@gmail.com>
2024-01-13 00:25:28 +0800

    update tabs for radiology

--- src/pages/Radiology-Reporting/components/details/buttonCard.tsx
+++ src/pages/Radiology-Reporting/components/details/buttonCard.tsx
@@ -2,7 +2,12 @@ import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
 import styles from '../../_radiology-reporting.module.scss'
 
-function ButtonCard() {
+type Props = {
+    label: string
+    onClick: () => void
+}
+
+function ButtonCard({ label, onClick }: Props) {
     return (
         <Box
             sx={{
@@ -16,8 +21,8 @@ function ButtonCard() {
                 },
             }}
         >
-            <Paper elevation={0} className={styles.card}>
-                <span className={styles.cardText}>Medical History</span>
+            <Paper elevation={0} className={styles.card} onClick={onClick}>
+                <span className={styles.cardText}>{label}</span>
             </Paper>
         </Box>
     )
--- src/pages/Radiology-Reporting/components/details/index.tsx
+++ src/pages/Radiology-Reporting/components/details/index.tsx
@@ -1,3 +1,4 @@
+import { Button, Divider, InputBase, Stack, Typography } from '@mui/material'
 import Box from '@mui/material/Box'
 import Tab from '@mui/material/Tab'
 import Tabs from '@mui/material/Tabs'
@@ -9,79 +10,221 @@ import UserHeader from '../user-header'
 import ButtonCard from './buttonCard'
 import Header from './header'
 
-type StyledTabsProps = {
-    children?: React.ReactNode
-    value: number
-    onChange: (event: React.SyntheticEvent, newValue: number) => void
-}
-
-const StyledTabs = styled((props: StyledTabsProps) => (
-    <Tabs
-        {...props}
-        TabIndicatorProps={{
-            children: <span className="MuiTabs-indicatorSpan" />,
-        }}
-    />
-))({
+const AntTabs = styled(Tabs)({
+    borderBottom: '1px solid #e8e8e8',
     '& .MuiTabs-indicator': {
-        display: 'flex',
-        justifyContent: 'center',
         backgroundColor: 'transparent',
     },
-    '& .MuiTabs-indicatorSpan': {
-        maxWidth: 40,
-        width: '100%',
-        backgroundColor: '#635ee7',
-    },
 })
-
 type StyledTabProps = {
     label: string
 }
-
-const StyledTab = styled((props: StyledTabProps) => (
+const AntTab = styled((props: StyledTabProps) => (
     <Tab disableRipple {...props} />
 ))(({ theme }) => ({
     textTransform: 'none',
-    fontWeight: theme.typography.fontWeightRegular,
-    fontSize: theme.typography.pxToRem(15),
-    marginRight: theme.spacing(1),
-    color: 'rgba(255, 255, 255, 0.7)',
+    minWidth: 0,
+    [theme.breakpoints.up('sm')]: {
+        minWidth: 0,
+    },
+    fontWeight: 500,
+    // marginRight: theme.spacing(1),
+    color: '#5A567B',
+    backgroundColor: '#DDDDDD',
+    boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.08)',
+    borderTopLeftRadius: '10px',
+    borderTopRightRadius: '10px',
+    '&:hover': {
+        color: '#40a9ff',
+        opacity: 1,
+    },
     '&.Mui-selected': {
-        color: '#fff',
+        color: '#211D4E',
+        fontWeight: 600,
+        background: '#FFF',
     },
     '&.Mui-focusVisible': {
-        backgroundColor: 'rgba(100, 95, 228, 0.32)',
+        backgroundColor: 'red',
     },
 }))
 
+type TabPanelProps = {
+    children?: React.ReactNode
+    dir?: string
+    index: number
+    value: number
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    )
+}
+
 function RadiologyReportDetail() {
+    //for tab
     const [value, setValue] = useState(0)
 
     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
         setValue(newValue)
     }
+
+    //for row button
+    const [isSelected, setIsSelected] = useState(0)
+
+    const handleButtonChange = (value: number) => {
+        setIsSelected(value)
+    }
+
     return (
         <>
             <ResponsiveAppBar />
             <UserHeader />
             <Header />
-            <Grid container>
-                <Grid md={3}>
-                    <ButtonCard />
+            <Grid container sx={{ background: '#EDEDED' }}>
+                <Grid md={3} textAlign={'center'}>
+                    <Stack
+                        direction="column"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        spacing={2}
+                    >
+                        <>
+                            <ButtonCard
+                                label="Medical History"
+                                onClick={() => console.log(1)}
+                            />
+                            <ButtonCard
+                                label="Allergy/ Drug Hx"
+                                onClick={() => console.log(1)}
+                            />
+                        </>
+
+                        <ButtonCard
+                            label="View Comment"
+                            onClick={() => console.log(1)}
+                        />
+                    </Stack>
                 </Grid>
                 <Grid md={9}>
-                    <Box sx={{ bgcolor: '#2e1534' }}>
-                        <StyledTabs
-                            value={value}
-                            onChange={handleChange}
-                            aria-label="styled tabs example"
-                        >
-                            <StyledTab label="Workflows" />
-                            <StyledTab label="Datasets" />
-                            <StyledTab label="Connections" />
-                        </StyledTabs>
-                        <Box sx={{ p: 3 }} />
+                    <Box sx={{ width: '80%' }}>
+                        <Box sx={{ bgcolor: 'transparent', mt: 5 }}>
+                            <AntTabs
+                                value={value}
+                                onChange={handleChange}
+                                aria-label="ant example"
+                            >
+                                <AntTab label="CT Scan" />
+                                <AntTab label="Mammography" />
+                                <AntTab label="X-Ray" />
+                                <AntTab label="Sonography" />
+                            </AntTabs>
+                            <Box sx={{ p: 3, bgcolor: '#fff' }}>
+                                <TabPanel value={value} index={0}>
+                                    <Grid container>
+                                        <Grid xs={12}>
+                                            <Button
+                                                variant="contained"
+                                                sx={{
+                                                    background:
+                                                        isSelected === 1
+                                                            ? '#fff'
+                                                            : '#EDEDED',
+                                                    boxShadow:
+                                                        '0px 3px 6px rgba(0, 0, 0, 0.10) !important',
+                                                    borderRadius: '5px',
+                                                    color:
+                                                        isSelected === 1
+                                                            ? '#211D4E'
+                                                            : '#5A567B',
+                                                    fontWeight: 600,
+                                                    marginRight: '1em',
+                                                }}
+                                                onClick={() =>
+                                                    handleButtonChange(1)
+                                                }
+                                            >
+                                                Exam. Report
+                                            </Button>
+                                            <Button
+                                                variant="contained"
+                                                sx={{
+                                                    background:
+                                                        isSelected === 0
+                                                            ? '#fff'
+                                                            : '#EDEDED',
+                                                    boxShadow:
+                                                        '0px 3px 6px rgba(0, 0, 0, 0.10) !important',
+                                                    borderRadius: '5px',
+                                                    color:
+                                                        isSelected === 0
+                                                            ? '#211D4E'
+                                                            : '#5A567B',
+                                                    fontWeight: 600,
+                                                }}
+                                                onClick={() =>
+                                                    handleButtonChange(0)
+                                                }
+                                            >
+                                                Comment & Suggestion
+                                            </Button>
+
+                                            <Divider light sx={{ my: 3 }} />
+                                        </Grid>
+                                        <Grid xs={12}>
+                                            <Typography
+                                                variant="h6"
+                                                sx={{
+                                                    color: '#219B8E',
+                                                    fontSize: '20px',
+                                                    fontWeight: 600,
+                                                }}
+                                            >
+                                                Overview
+                                            </Typography>
+                                        </Grid>
+                                        <Grid xs={12}>
+                                            <InputBase
+                                                id="outlined-search"
+                                                placeholder="Search..."
+                                                type="search"
+                                                sx={{
+                                                    background: '#F9F9F9',
+                                                    boxShadow:
+                                                        '0px 2px 2px 1px rgba(0, 0, 0, 0.05) inset',
+                                                    borderRadius: '5px',
+                                                    minWidth: '100%',
+                                                    padding: '10px 15px',
+                                                }}
+                                            />
+                                        </Grid>
+                                    </Grid>
+                                </TabPanel>
+                                <TabPanel value={value} index={1}>
+                                    Item Two
+                                </TabPanel>
+                                <TabPanel value={value} index={2}>
+                                    Item Three
+                                </TabPanel>
+                                <TabPanel value={value} index={3}>
+                                    Item Four
+                                </TabPanel>
+                            </Box>
+                        </Box>
                     </Box>
                 </Grid>
             </Grid>
