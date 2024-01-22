0071a15e915c8a4391a6018279c1af5f7670d7a5
JX <jxyee981101@gmail.com>
2024-01-23 00:43:05 +0800

    screening room table component

---
+++ src/types/README.md
@@ -0,0 +1,1 @@
+Temporary put the types here.
---
+++ src/types/ScreeningRoom/WaitingListData.ts
@@ -0,0 +1,10 @@
+export type WaitingListData = {
+    id: number
+    seqNo: string
+    name: string
+    age: number
+    gender: string
+    package: string
+    checkInAt?: string
+    waitingAt: string
+}
