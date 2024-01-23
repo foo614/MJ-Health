87f2a57ea33b3f8f18bbac6d20491c6f90d768c6
JX <jxyee981101@gmail.com>
2024-01-24 02:47:16 +0800

    update import

--- src/pages/Bone-Mineral-Density-Info/index.tsx
+++ src/pages/Bone-Mineral-Density-Info/index.tsx
@@ -1,7 +1,7 @@
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import ResponsiveAppBar from 'components/AppBar'
-import ScreeningRoomInfoHeader from 'components/ScreeningRoom/ScreeningRoomInfoHeader'
+import ScreeningRoomInfoHeader from 'components/ScreeningRoom/InfoHeader'
 import { BONE_MINERAL_DENSITY_PAGE } from 'constant'
 
 const BoneMineralDensityInfo = () => {
